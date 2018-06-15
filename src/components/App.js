e0a21c86b37fcd01d4ec45d8a9efe2eb4548c00d
sleeplesseditor <josh@bryson.me.uk>
2018-06-15 17:10:55 +0100

    Keys updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,11 @@ import '../style/App.css';
 import firebase from 'firebase';
 
 import Header from './Header';
+import { FirebaseConfig } from '../config/keys';
 
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: "AIzaSyAWdtOXxCLQn4W54on1c2OZ5pbrd1Rfpbc",
-      authDomain: "filmlist-eecc7.firebaseapp.com",
-      databaseURL: "https://filmlist-eecc7.firebaseio.com",
-      projectId: "filmlist-eecc7",
-      storageBucket: "filmlist-eecc7.appspot.com",
-      messagingSenderId: "902688634012"
-    };
-
-    firebase.initializeApp(config);
+    firebase.initializeApp(FirebaseConfig);
   }
 
   render() {
---
+++ src/config/dev.js
@@ -0,0 +1,10 @@
+//Prod.js - do not commit
+
+export const FirebaseConfig = {
+    apiKey: "AIzaSyAWdtOXxCLQn4W54on1c2OZ5pbrd1Rfpbc",
+    authDomain: "filmlist-eecc7.firebaseapp.com",
+    databaseURL: "https://filmlist-eecc7.firebaseio.com",
+    projectId: "filmlist-eecc7",
+    storageBucket: "filmlist-eecc7.appspot.com",
+    messagingSenderId: "902688634012"
+};
